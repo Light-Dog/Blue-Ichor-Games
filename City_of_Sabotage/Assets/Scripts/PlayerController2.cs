b2e609609d35a192f7b81b9bd15868da6e98dbfd
Weston Baker <wbaker_98@live.com>
2020-09-02 13:57:50 -0700

    Started Improved Character Controller in a new level

---
+++ City_of_Sabotage/Assets/Scripts/PlayerController2.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController2 : MonoBehaviour
+{
+    public float moveSpeed;
+
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xSpeed = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+        float zSpeed = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+
+        rb.velocity = new Vector3(xSpeed, rb.velocity.y, zSpeed);
+    }
+}
