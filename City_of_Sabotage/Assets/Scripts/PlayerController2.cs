d2a650a690a5a59bf3b5d36967215c43926fa348
Weston Baker <wbaker_98@live.com>
2020-09-02 14:23:42 -0700

    Character Controller 2.0

--- City_of_Sabotage/Assets/Scripts/PlayerController2.cs
+++ City_of_Sabotage/Assets/Scripts/PlayerController2.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 public class PlayerController2 : MonoBehaviour
 {
     public float moveSpeed;
+    public float jumpForce;
 
-    Rigidbody rb;
+    public Rigidbody rb;
 
     // Start is called before the first frame update
     void Start()
@@ -17,9 +18,20 @@ public class PlayerController2 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float xSpeed = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
-        float zSpeed = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        Mover();
+    }
+
+    void Mover()
+    {
+        float xSpeed = Input.GetAxis("Horizontal") * moveSpeed;// * Time.deltaTime;
+        float zSpeed = Input.GetAxis("Vertical") * moveSpeed;// * Time.deltaTime;
 
         rb.velocity = new Vector3(xSpeed, rb.velocity.y, zSpeed);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.velocity = new Vector3(xSpeed, jumpForce, zSpeed);
+        }
     }
+
 }
