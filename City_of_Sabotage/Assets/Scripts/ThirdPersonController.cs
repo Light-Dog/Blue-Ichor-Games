b5e2c417ab2304400bfcd22f2ac94571c7aa0a0d
Weston Baker <wbaker_98@live.com>
2020-09-08 15:13:00 -0700

    Started work on ANOTHER character contorller
    This one has better camera movement and player turning
    Needs: Jumping, Animations

---
+++ City_of_Sabotage/Assets/Scripts/ThirdPersonController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonController : MonoBehaviour
+{
+    public float moveSpeed;
+    public float turnSmoother = .1f;
+
+    public CharacterController controller;
+    public Transform cam;
+
+    float turnVelocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float vertical = Input.GetAxisRaw("Vertical");
+        float horizontal = Input.GetAxisRaw("Horizontal");
+
+        Vector3 direction = new Vector3(horizontal, 0.0f, vertical).normalized;
+
+        if(direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnVelocity, turnSmoother);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDir.normalized * moveSpeed * Time.deltaTime);
+        }
+    }
+}
