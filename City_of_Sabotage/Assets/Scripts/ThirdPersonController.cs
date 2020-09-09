f868da935a933a52c08d300af2a3bc2594319fb0
Weston Baker <wbaker_98@live.com>
2020-09-08 17:07:27 -0700

    Fixed our player controller
    well with the exception of animations but that is the next step

--- City_of_Sabotage/Assets/Scripts/ThirdPersonController.cs
+++ City_of_Sabotage/Assets/Scripts/ThirdPersonController.cs
@@ -5,12 +5,24 @@ using UnityEngine;
 public class ThirdPersonController : MonoBehaviour
 {
     public float moveSpeed;
+    public float jumpForce;
+    public float gravityScale;
     public float turnSmoother = .1f;
 
     public CharacterController controller;
     public Transform cam;
 
     float turnVelocity;
+    bool doubleJump = false;
+    float y_value;
+    Vector3 direction;
+    Vector3 jump;
+
+    //range of (0, 1.0f)
+    public float HorizontalAnimSmoothTime = 0.2f;
+    public float VerticalAnimTime = 0.2f;
+    public float StartAnimTime = 0.3f;
+    public float StopAnimTime = 0.15f;
 
     // Start is called before the first frame update
     void Start()
@@ -21,19 +33,46 @@ public class ThirdPersonController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        playerMovement();
+    }
+
+    void playerMovement()
+    {
         float vertical = Input.GetAxisRaw("Vertical");
         float horizontal = Input.GetAxisRaw("Horizontal");
 
-        Vector3 direction = new Vector3(horizontal, 0.0f, vertical).normalized;
+        if(controller.isGrounded)
+        {
+            jump.y = -1.0f;
 
-        if(direction.magnitude >= 0.1f)
+            if(Input.GetButtonDown("Jump"))
+            {
+                jump.y = jumpForce;
+                doubleJump = true;
+            }
+        }
+        else if(Input.GetButtonDown("Jump") && doubleJump)
+        {
+            jump.y = jumpForce;
+            doubleJump = false;
+        }
+
+        direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if (direction.magnitude >= 0.1f)
         {
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnVelocity, turnSmoother);
+            //rotate the player based on input
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            controller.Move(moveDir.normalized * moveSpeed * Time.deltaTime);
+            moveDir = moveDir.normalized * moveSpeed;
+
+            controller.Move(moveDir * Time.deltaTime);
         }
+
+        jump.y += (Physics.gravity.y * gravityScale * Time.deltaTime);
+        controller.Move(jump * Time.deltaTime);
     }
 }
