2b8856a139747ed65f5fb4cdcd7a29c05a642afb
Ben <b.albers@digipen.edu>
2020-10-15 13:51:32 -0700

    Gear Spin script, more colliders

---
+++ City_of_Sabotage/Assets/Scripts/CoinSpin.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinSpin : MonoBehaviour
+{
+    public float yAngle;
+    private float xAngle, zAngle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (yAngle == 0f || yAngle == 1.1f)
+        yAngle = 1.1f;
+        xAngle = 0.0f;
+        zAngle = 0.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //spin the coin
+        this.transform.Rotate(xAngle, yAngle, zAngle, Space.Self);
+    }
+}
