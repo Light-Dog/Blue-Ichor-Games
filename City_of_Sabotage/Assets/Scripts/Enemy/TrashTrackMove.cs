939a65b6df49f675694736a19d4b8f9332c167dd
Ben <b.albers@digipen.edu>
2020-10-28 14:45:06 -0700

    CITY resetpos attempt

--- City_of_Sabotage/Assets/Scripts/Enemy/TrashTrackMove.cs
+++ City_of_Sabotage/Assets/Scripts/Enemy/TrashTrackMove.cs
@@ -28,15 +28,6 @@ public class TrashTrackMove : MonoBehaviour
         float y_trans = transform.position.y;
         transform.position = Vector3.MoveTowards(transform.position, target.position, step);
         transform.position = new Vector3(transform.position.x, y_trans, transform.position.z);
-        
-
-        //move towards
-        /*
-        if (transform.position.y < target.position.y)
-        {
-            transform.position = new Vector3(transform.position.x, target.position.y, transform.position.z);
-        }
-        */
 
         //turn facing
         Quaternion targetRotation = Quaternion.LookRotation(target.position - transform.position);
---
+++ City_of_Sabotage/Assets/Scripts/ResetPos.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetPos : MonoBehaviour
+{
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = FindObjectOfType<AmeliaStats>().gameObject;
+        player.transform.position = this.gameObject.transform.position;
+    }
+
+    private void Update()
+    {
+        player.transform.position = this.gameObject.transform.position;
+    }
+
+}
