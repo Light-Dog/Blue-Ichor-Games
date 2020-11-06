1050e4de06678afe1b3849c8348249863aa829bd
Ben <b.albers@digipen.edu>
2020-11-06 14:46:04 -0800

    Key and Door
    Modeled, colored, animated

---
+++ City_of_Sabotage/Assets/Scripts/AnimControl.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimControl : MonoBehaviour
+{
+    public Animation anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animation>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void StopAnim()
+    {
+        if (anim != null)
+        {
+            anim.Stop();
+        }
+    }
+
+    void StartAnim()
+    {
+        if (anim != null)
+        {
+            anim.Play();
+        }
+    }
+}
