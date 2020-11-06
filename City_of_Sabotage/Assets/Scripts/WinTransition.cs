0f86c360998d07cec5b333e6b1302cfc979f5221
Ben <b.albers@digipen.edu>
2020-11-05 17:34:12 -0800

    Ending

---
+++ City_of_Sabotage/Assets/Scripts/WinTransition.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinTransition : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        //if (collision.gameObject.GetComponent<BoxCollider>())
+        {
+            SceneManager.LoadScene(3);
+        }
+    }
+}
