dfa2b8275956ee42bb12814143e9eefad393d247
Ben <b.albers@digipen.edu>
2020-11-06 16:16:24 -0800

    Door opens properly, minigun spins again
    all problems fixed ezpz

--- City_of_Sabotage/Assets/Scripts/DoorMat.cs
+++ City_of_Sabotage/Assets/Scripts/DoorMat.cs
@@ -6,6 +6,9 @@ public class DoorMat : MonoBehaviour
 {
     public GameObject doorhinge;
     public GameObject player;
+    public float range = 15.0f;
+    public KeyCode openKey = KeyCode.O;
+    public bool opened = false;
 
     // Start is called before the first frame update
     void Start()
@@ -16,14 +19,35 @@ public class DoorMat : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-    }
 
-    private void OnCollisionEnter(Collision collision)
-    {
-        //if (collision.gameObject.GetComponent<AmeliaStats>())
+        if (opened == false)
         {
-            doorhinge.GetComponent<AnimControl>().StartAnim();
+            //open with "O" button
+            if (Input.GetKeyDown(openKey))
+            {
+                if (1 == 1) // if player has the key
+                {
+                    if (Vector3.Distance(gameObject.transform.position, player.transform.position) < range)
+                    {
+                        doorhinge.GetComponent<AnimControl>().StartAnim();
+                        opened = true;
+                    }
+                }
+            }
+
+            //open in range with key
+            if (Vector3.Distance(gameObject.transform.position, player.transform.position) < range)
+            {
+                if (1 == 1) // if player has the key
+                {
+                    doorhinge.GetComponent<AnimControl>().StartAnim();
+                    opened = true;
+                }
+            }
+        }
+        else
+        { 
+            //nothing for now
         }
     }
 
